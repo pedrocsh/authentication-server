5e4ab3016b8487dc30834397183d68f7e2d3b04b
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-15 14:22:13 -0300

    config cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(PORT, () => {
